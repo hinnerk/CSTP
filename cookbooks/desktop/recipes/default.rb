4cc8b93a7066403c372769d0d1d8c1550ce69b0e
Hinnerk <hinnerk@randnotizen.de>
2011-01-08 19:48:10 +0100

    Added recipe local cache, which expects a caching http proxy running on 192.168.23.1:8123.

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -1,6 +1,7 @@
 # vagrant desktop main cookbook
 # This cookbook includes and sets up a remote desktop server.
 #
+require_recipe "local-cache"
 require_recipe "apt"
 require_recipe "chromium-browser"
 # require_recipe "firefox"
