17ae852b25471d3b3908dcb8a3182dcbc7794616
Hinnerk <hinnerk@randnotizen.de>
2011-01-11 15:30:17 +0100

    ifdown and ifup temporarily disabled

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -1,7 +1,10 @@
 # vagrant desktop main cookbook
 # This cookbook includes and sets up a remote desktop server.
 #
+# require_recipe "ifup"
 require_recipe "apt"
-require_recipe "chromium-browser"
+# require_recipe "chromium-browser"
 # require_recipe "firefox"
-require_recipe "ifdown"
+require_recipe "google-chrome"
+require_recipe "global-proxy"
+# require_recipe "ifdown"
