009d7af5235fcc67ecfad0095f45c7c49ffebf6d
Hinnerk <hinnerk@randnotizen.de>
2011-01-12 14:04:14 +0100

    Disabled local-cache as most installations won't have a local cache running on 192.168.32.1:8123.

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -2,7 +2,7 @@
 # This cookbook includes and sets up a remote desktop server.
 #
 # require_recipe "ifup"
-require_recipe "local-cache"
+#require_recipe "local-cache"
 require_recipe "apt"
 # require_recipe "chromium-browser"
 # require_recipe "firefox"
